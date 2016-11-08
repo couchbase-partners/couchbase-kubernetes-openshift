4af4ef0711019898c740bb81278b88b8ae8701f0
Christian Simon <simon@swine.de>
2016-11-08 10:09:00 +0000

    Use new image with fixed signal handling

--- download_import_image.sh
+++ download_import_image.sh
@@ -3,18 +3,13 @@
 set -x
 set -e
 
-IMAGE_ID=afaf32cb629c
-IMAGE_ID2=fe297a796d97
+IMAGE_ID=00e4d95aa26c
 
 # download image
-test -e rhel72_cb451.tar || curl -O https://s3-us-west-1.amazonaws.com/cb-openshift/rhel72_cb451.tar
-
-# download image2
-test -e os-cb.tar || curl -O https://s3-eu-west-1.amazonaws.com/os-cb-image/os-cb.tar
+test -e os-cb.tar || curl -o os-cb.tar https://s3-eu-west-1.amazonaws.com/os-cb-image/os-cb-0.2.tar
 
 # Load into local docker
-docker inspect $IMAGE_ID > /dev/null || docker load -i rhel72_cb451.tar
-docker inspect $IMAGE_ID2 > /dev/null || docker load -i os-cb.tar
+docker inspect $IMAGE_ID > /dev/null || docker load -i os-cb.tar
 
 # Authorize user to be able to push
 oc policy add-role-to-user admin admin -n openshift
@@ -27,14 +22,10 @@ TOKEN=$(oc whoami -t)
 unset KUBECONFIG
 
 # Push into local registry
-REGISTRY_IP=$(kubectl get svc docker-registry -o jsonpath={.spec.clusterIP})
+REGISTRY_IP=$(kubectl get svc docker-registry -o "jsonpath={.spec.clusterIP}")
 # Get from https://openshift.jetstack.net:8443/console/command-line
-docker login -u admin -e tech@jetstack.io -p ${TOKEN} "${REGISTRY_IP}:5000"
-IMAGE_NAME="${REGISTRY_IP}:5000/openshift/couchbase:4.5.1-enterprise"
-docker tag $IMAGE_ID $IMAGE_NAME
-docker push $IMAGE_NAME
+docker login -u admin -e tech@jetstack.io -p "${TOKEN}" "${REGISTRY_IP}:5000"
 
 IMAGE_NAME="${REGISTRY_IP}:5000/openshift/couchbase-noroot:4.5.1-enterprise"
-docker tag $IMAGE_ID2 $IMAGE_NAME
-docker push $IMAGE_NAME
-
+docker tag "$IMAGE_ID" "$IMAGE_NAME"
+docker push "$IMAGE_NAME"
