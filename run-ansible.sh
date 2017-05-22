ab94cb7a3fe67ce3bef1446719ef5a6fbedb83dc
Christian Simon <simon@swine.de>
2017-05-22 12:00:07 +0000

    Deploy 1.5.0 release

--- run-ansible.sh
+++ run-ansible.sh
@@ -22,6 +22,7 @@ fi
 
 VARS="cluster_id=jetstack"
 VARS="${VARS} cluster_env=dev"
+VARS="${VARS} openshift_version=1.5.0"
 VARS="${VARS} deployment_type=${deployment_type}"
 VARS="${VARS} openshift_cloudprovider_kind=aws"
 VARS="${VARS} openshift_cloudprovider_aws_access_key=$(echo "${terraform_output}" | jq -r ".iam_access_key.value")"
